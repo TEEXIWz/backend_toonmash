f208730833fdbc20303b8a7a4ebfa48093ff32ef
TEEXIWz <64011212136@msu.ac.th>
2024-03-11 13:27:36 +0700

    update vote

--- api/vote.ts
+++ api/vote.ts
@@ -37,7 +37,7 @@ router.get('/total', (req, res)=>{
 router.get('/date', (req, res)=>{
     if (req.query.id) {
         // conn.query('SELECT pid,SUM(score) as totalScore,dates.fulldate FROM dates LEFT JOIN vote ON dates.fulldate = vote.voted_at WHERE pid = ? and dates.fulldate BETWEEN adddate(now(),-7) and now() GROUP BY DATE(voted_at) ORDER BY DATE(voted_at) desc',req.query.id, (err,result)=>{
-        conn.query('SELECT SUM(score) as totalScore,DATE(voted_at) as date FROM vote WHERE pid = ? and voted_at BETWEEN DATE_SUB(NOW(),INTERVAL 7 DAY) and now() GROUP BY DATE(voted_at) ORDER BY DATE(voted_at) desc',req.query.id, (err,result)=>{
+        conn.query('SELECT pid,1000+SUM(CASE WHEN v.winner = pid THEN v.scoreWin ELSE 0 END)+SUM(CASE WHEN v.loser = pid THEN v.scoreLose ELSE 0 END) as totalScore,DATE(voted_at) as date FROM picture LEFT JOIN vote v ON (v.winner = pid OR v.loser = pid) WHERE pid = ? and voted_at BETWEEN DATE_SUB(NOW(),INTERVAL 7 DAY) and now() GROUP BY DATE(voted_at) ORDER BY DATE(voted_at) desc',req.query.id, (err,result)=>{
             if (err) {
                 res.status(500).json(err)
             }
